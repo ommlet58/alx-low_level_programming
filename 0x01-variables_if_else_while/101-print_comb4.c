99bfccd77e2d4de3655fdda95cfe85860ae47e03
ommlet58 <hichamsskouti@gmail.com>
2023-03-17 14:28:41 -0700

    mm

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	int num1, num2, num3;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+		{
+			for (num3 = 0; num3 < 10; num3++)
+			{
+				if (num1 < num2 && num2 < num3)
+				{
+					putchar((num1 % 10) + '0');
+					putchar((num2 % 10) + '0');
+					putchar((num3 % 10) + '0');
+					if (num1 == 7 && num2 == 8 && num3 == 9)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
