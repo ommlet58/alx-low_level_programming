f0acd372b0ac383b100eee55dd6c9a90317a58f6
ommlet58 <hichamsskouti@gmail.com>
2023-03-17 12:01:58 -0700

    4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26 ; i++)
+	{
+		putchar(tolower(a[i]));
+		putchar(toupper(a[i]));
+	}
+	putchar('\n');
+	return (0);
+}
