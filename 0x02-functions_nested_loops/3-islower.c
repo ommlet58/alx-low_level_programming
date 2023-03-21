f1d86c1c02a733bef9386dc35085923491078376
ommlet58 <hichamsskouti@gmail.com>
2023-03-21 14:48:51 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  _islower  - check the code.
+ * @c: The character to be checked
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (islower(c))
+		return (1);
+	else
+		return (0);
+}
