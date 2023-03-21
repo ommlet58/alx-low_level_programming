a45e1ebf89c83cf34939df3bfd4d383f9147af15
ommlet58 <hichamsskouti@gmail.com>
2023-03-21 14:57:34 -0700

    chech for alphabtic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha  - check the code.
+ * @c: The character to be checked
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+	if (c <= 'Z' && c <= 'a')
+		return (1);
+	else
+		return (0);
+}
+
