3bee1a514c4786e83710bef239fda0b85bf5fd76
ommlet58 <hichamsskouti@gmail.com>
2023-03-21 16:21:15 -0700

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 time tabel.
+ *
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10 ; j++)
+		{
+			_putchar(' ');
+			_putchar((i * j));
+			_putchar(',');
+		}
+	_putchar('\n');
+	}
+}
