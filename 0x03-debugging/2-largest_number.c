f5e70213785e87363cc23bac56d6b3f6901ab376
ommlet58 <hichamsskouti@gmail.com>
2023-03-22 15:36:00 -0700

    hot fix

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,8 +11,12 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-	
-	if (a > b && a > c)
+
+	if (a > b && b > c)
+	{
+		largest = a;
+	}
+	else if (a > c && c > b)
 	{
 		largest = a;
 	}
@@ -20,10 +24,18 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else
+	else if (b > c && c > a)
+	{
+		largest = b;
+	}
+	else if (c > a && a > b)
 	{
 		largest = c;
 	}
-	
+	else if (c > b && b > a)
+	{
+		largest = c;
+	}
+
 	return (largest);
 }
