e64bfcef35e608a0ccc20756303be419e177b261
ommlet58 <hichamsskouti@gmail.com>
2023-03-23 15:36:50 -0700

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main -prime factors of 1231952.
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	long prime = 612852475143, div;
+
+	while (div < (prime / 2))
+	{
+		if ((prime % 2) == 0)
+		{
+			prime /= 2;
+			continue;
+		}
+
+		for (div = 3; div < (prime / 2); div += 2)
+		{
+			if ((prime % div) == 0)
+				prime /= div;
+		}
+	}
+
+	printf("%ld\n", prime);
+
+	return (0);
+}
+
+
