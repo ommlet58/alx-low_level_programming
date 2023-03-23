607855ce7a93bbac0305c7718004840e8cf1936f
ommlet58 <hichamsskouti@gmail.com>
2023-03-23 07:54:52 -0700

    nimbers 10 time

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,13 +5,15 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int num, count;
 
-	for (i = 0; i < 10; i++)
+	for (count = 0; count <= 9; count++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (num = 0; num <= 14; num++)
 		{
-			_putchar(j + '0');
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
 		}
 		_putchar('\n');
 	}
