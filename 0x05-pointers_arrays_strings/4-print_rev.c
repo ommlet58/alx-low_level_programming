c03f684266f00967497a49682c1ff49af1e49af2
ommlet58 <hichamsskouti@gmail.com>
2023-03-27 16:57:11 -0700

    print string in revrse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,14 +6,13 @@
  */
 void print_rev(char *s)
 {
-	int len = 0;
-	int index;
+	int len = 0, index;
 
-	while (*s)
-	{
+	while (s[index++])
 		len++;
-	}
+
 	for (index = len - 1; index >= 0; index--)
 		_putchar(s[index]);
+
 	_putchar('\n');
 }
