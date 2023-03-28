6f8d8766dd3bc2eadef40a448e62e1aa6d070a50
ommlet58 <hichamsskouti@gmail.com>
2023-03-27 17:03:37 -0700

    print

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,12 +6,12 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int index;
 
-	while (s[index++])
-		len++;
+	while (s[index] != '\0')
+		index++;
 
-	for (index = len - 1; index >= 0; index--)
+	for (index; index >= 0; index--)
 		_putchar(s[index]);
 
 	_putchar('\n');
