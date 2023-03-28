8f05aabf76d53dde6948b3390faeb46f4e35bc6f
ommlet58 <hichamsskouti@gmail.com>
2023-03-27 17:09:50 -0700

    pront revrse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_rev -print a string in revrse.
@@ -6,13 +7,22 @@
  */
 void print_rev(char *s)
 {
-	int index;
+	int c = 0;
 
-	while (s[index] != '\0')
-		index++;
 
-	for (index -= 1; index >= 0; index--)
-		_putchar(s[index]);
 
+
+
+
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
 	_putchar('\n');
 }
