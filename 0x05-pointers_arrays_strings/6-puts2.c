ebda4435f2396a45e2136c26aca945d9e294fcec
ommlet58 <hichamsskouti@gmail.com>
2023-03-27 17:26:36 -0700

    print string revrse

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 -prints every other character of a string.
+ * @str: string.
+ */
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		if (a % 2 != 0)
+		{
+			_putchar(str[a - 1]);
+		}
+		a++;
+	}
+	_putchar('\n');
+}
+
