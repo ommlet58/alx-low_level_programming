20183ccad5ee7069f06c6bd0d71c5a46aadf5822
ommlet58 <hichamsskouti@gmail.com>
2023-03-27 18:27:26 -0700

    copies the string pointed to by src

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,15 +8,14 @@
  *
  * Return: A pointer to the destination string @dest.
  */
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
-
-	while (src[index])
+	int i;
+	
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[index] = src[index];
-		index++;
+		dest[i] = src[i];
 	}
-
+	dest[i++] = '\0';
 	return (dest);
 }
