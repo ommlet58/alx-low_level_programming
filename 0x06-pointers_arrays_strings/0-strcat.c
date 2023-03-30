1b976840b4feeb4a86966c921980afdf78a1e533
ommlet58 <hichamsskouti@gmail.com>
2023-03-30 14:00:15 -0700

    concat string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,6 +8,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	*dest = *dest + " " + *src;
-	return (*dest);
+	int i = 0, dest_len = 0;
+
+	while (dest[i++])
+		dest_len++;
+
+	for (i = 0; src[i]; i++)
+		dest[dest_len++] = src[i];
+
+	return (dest);
 }
