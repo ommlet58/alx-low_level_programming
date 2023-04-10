1609146e5a7cbaf250e562e9cb915a00137e19a6
ommlet58 <hichamsskouti@gmail.com>
2023-04-10 16:42:24 -0700

    multiple two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -mutiplies two numbers
+ * @argc: number of parametrs
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int m = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			m = m * atoi(argv[i]);
+		}
+		printf("%d \n", m);
+	} else
+		printf("Error\n");
+	return (0);
+}
