bc0a2477737548ae71370efa8cd51aac4065a3dd
ommlet58 <hichamsskouti@gmail.com>
2023-04-27 14:27:37 -0700

    Task 4 - Free a list_t list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,24 +1,17 @@
-/*
- * File: 4-free_list.c
- * Auth: Brennan D Baraban
- */
-
-#include "lists.h"
 #include <stdlib.h>
+#include "lists.h"
 
 /**
- * free_list - Frees a list_t list.
- * @head: A pointer to the list_t list.
+ * free_list - Free all malloced spaced of a list
+ * @head: Pointer to the start of the list
  */
 void free_list(list_t *head)
 {
-	list_t *tmp;
-
-	while (head)
+	if (head != NULL)
 	{
-		tmp = head->next;
+		if (head->next != NULL)
+			free_list(head->next);
 		free(head->str);
 		free(head);
-		head = tmp;
 	}
 }
